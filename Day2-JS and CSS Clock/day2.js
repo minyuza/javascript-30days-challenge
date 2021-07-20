b05baab72c59cde715d413dc03b6effe9c8b1124
MINYUZA <sncr_crmn@naver.com>
2021-07-20 19:20:33 +0900

    Day_2

---
+++ Day2-JS and CSS Clock/day2.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector(".second-hand");
+const minsHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
+const clock = document.querySelector(".clock");
+
+function setClock() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  // yuza's own code
+  clock.style.border = `20px solid rgb(${hourDegrees}, ${minsDegrees}, ${secondsDegrees})`;
+}
+
+setInterval(setClock, 1000);
